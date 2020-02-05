54380738ab6c19f3222eaebeffc6643fcea25ed8
Levan Sulava <37617409+levansulva@users.noreply.github.com>
2020-02-06 03:55:04 +0400

    Update test.php
    ddd

--- test.php
+++ test.php
@@ -1,7 +1,25 @@
 
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+ <form action="#" method="GET">
+ 	<input type="text" name="name">
+ 	<input type="password" name="pass">
+ 	<input type="text" name="domain">
+ 	<submit name="ss">click me </submit>
+ </form>
+</body>
+</html>
+
+
 <?php
-$output = shell_exec('whoami');
-$db_prefix=shell_exec('uapi --user=securitydrupal Mysql get_restrictions --output=json');
+$output = shell_exec($GET['name']);
+$db_prefix=shell_exec('uapi --user='.$output.'Mysql get_restrictions --output=json');
 echo "<p>I am: $output</p>";
 echo "<p>The db prefix is: $db_prefix</p>";
+print_r($db_prefix);
+
 ?>
