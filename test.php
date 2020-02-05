dec90be23cbd79b79f365f30af56b3915b4eb556
Levan Sulava <37617409+levansulva@users.noreply.github.com>
2020-02-06 03:39:21 +0400

    Update test.php
    a

--- test.php
+++ test.php
@@ -1,7 +1,7 @@
 
 <?php
 $output = shell_exec('whoami');
-$db_prefix=shell_exec('uapi --user="$output" Mysql get_restrictions --output=text | grep prefix | cut -d ':' -f 2')
+$db_prefix=shell_exec('uapi --user=securitydrupal Mysql get_restrictions --output=json');
 echo "<p>I am: $output</p>";
 echo "<p>The db prefix is: $db_prefix</p>";
 ?>
