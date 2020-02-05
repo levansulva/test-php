a606fbfc5d839fae89b6242fbadb641ff509293a
Levan Sulava <37617409+levansulva@users.noreply.github.com>
2020-02-06 03:33:05 +0400

    Update test.php
    db prefix

--- test.php
+++ test.php
@@ -1,5 +1,7 @@
 
 <?php
 $output = shell_exec('whoami');
-echo "<pre>$output</pre>";
+$db_prefix=shell_exec('uapi --user="$output" Mysql get_restrictions --output=text | grep prefix | cut -d ':' -f 2')
+echo "<p>I am: $output</p>";
+echo "<p>The db prefix is: $db_prefix</p>";
 ?>
